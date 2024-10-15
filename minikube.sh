a5ee54a936e101f7314fef6e7b75588ce1e513ba
Debarghya Sharma <68813537+debarghyasarma@users.noreply.github.com>
2024-10-16 00:17:12 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 sudo apt install -y curl wget apt-transport-https
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
